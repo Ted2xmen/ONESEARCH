22ef01a147c34ee07c6fb082049cec8eb640e007
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-06-10 12:41:19 +0300

    first commit

---
+++ allsearch.js
@@ -0,0 +1,44 @@
+
+
+ document.querySelector(".btn").addEventListener('click' , function() {
+
+     let userInput = document.querySelector(".userText").value;
+       // alert(userInput);
+    
+
+ });
+
+
+
+// let googleUrl = "'<a href="'https://yandex.com.tr/search/?text=kol+saati" target="_blank">Y</a>"';
+
+//         // console.log(googleUrl);
+//         console.log(googleUrl);
+
+// let googleHtml = document.querySelector(".google");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector(".btn").addEventListener('click' , function(el) {
+
+//     alert("works");
+//     return el;
+
+// });
+
+
