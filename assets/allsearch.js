8d3fc97f75e6ddaab190f28dc8121d15fb097063
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-06-15 15:35:05 +0300

    added: new buttons and styles

--- assets/allsearch.js
+++ assets/allsearch.js
@@ -84,9 +84,9 @@ linkedin.href = linkedinURL
 
 
 // who.is
-let whoisURL = "https://who.is/whois/" + oneSearch;
-let whois = document.querySelector("#whois");
-whois.href = whoisURL;
+// let whoisURL = "https://who.is/whois/" + oneSearch;
+// let whois = document.querySelector("#whois");
+// whois.href = whoisURL;
 
 // Github
 let githubURL = "https://github.com/search?q=" + oneSearch;
@@ -169,11 +169,34 @@ devto.href = devtoURL;
 
 });
 
-
-
-
-
-
+// for bookmark
+function rudr_favorite(a) {
+	pageTitle=document.title;
+	pageURL=document.location;
+	try {
+		// Internet Explorer solution
+		eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g,''));
+	}
+	catch (e) {
+		try {
+			// Mozilla Firefox solution
+			window.sidebar.addPanel(pageTitle, pageURL, "");
+		}
+		catch (e) {
+			// Opera solution
+			if (typeof(opera)=="object") {
+				a.rel="sidebar";
+				a.title=pageTitle;
+				a.url=pageURL;
+				return true;
+			} else {
+				// The rest browsers (i.e Chrome, Safari)
+				alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+			}
+		}
+	}
+	return false;
+}
 
 
 
